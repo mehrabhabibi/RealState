335da00a7ae752fdf1c44a1d603caa287bc76583
mehrabhabibi <mehrab.habibi@gmail.com>
2025-07-10 16:35:21 +0330

    Fixed gitignore

--- RealState.Infrastructure/obj/Debug/net8.0/RealState.Infrastructure.AssemblyInfo.cs
+++ RealState.Infrastructure/obj/Debug/net8.0/RealState.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealState.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb69bfeb714b3d474da4abfd21b9c0fe2b59d26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532a3dc8595e734eef71f71ae69019abfa9af797")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealState.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealState.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
