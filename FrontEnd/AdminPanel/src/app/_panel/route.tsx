3865f35f94144d5a06965fcd48aea3293245955a
parsa-yz-w <parsa.yz@apptegral.com>
2026-01-05 06:39:40 +0330

    Refactor: add commented for futere login validation

--- FrontEnd/AdminPanel/src/app/_panel/route.tsx
+++ FrontEnd/AdminPanel/src/app/_panel/route.tsx
@@ -2,6 +2,20 @@ import { Outlet, createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_panel')({
   component: PathlessLayoutComponent,
+  // beforeLoad: ({ context, location }) => {
+  //   // Replace this with your actual auth check logic
+  //   const isAuthenticated = false 
+
+  //   if (!isAuthenticated) {
+  //     throw redirect({
+  //       to: '/login',
+  //       search: {
+  //         // Keep track of where they were trying to go
+  //         redirect: location.href,
+  //       },
+  //     })
+  //   }
+  // },
 })
 
 function PathlessLayoutComponent() {
